1f6adf82f88fda88ec3cfefdd1272934dbae5982
Paulo <124280407+PDSentinella@users.noreply.github.com>
2025-03-25 15:47:56 +0000

    connectToDataBase

--- Models/MotasAlcoaContext.cs
+++ Models/MotasAlcoaContext.cs
@@ -1,13 +1,24 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
-using motasAlcoa.Models;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using motasAlcoafinal.Models;
 
 namespace MotasAlcoafinal.Models
 {
     public class MotasAlcoaContext : DbContext
     {
         public MotasAlcoaContext(DbContextOptions<MotasAlcoaContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if (!optionsBuilder.IsConfigured)
+            
+                //optionsBuilder.UseSqlServer("Server=tcp:servidormotasalcoa.database.windows.net,1433;Database=motasAlcoa;User Id=motasAlcoa;Password=THEWEB123#;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;", 
+                //    sqlOptions => sqlOptions.EnableRetryOnFailure());
+            
 
+            optionsBuilder.ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning));
+
+        }
         public DbSet<Clientes> Clientes { get; set; }
         public DbSet<Pecas> Pecas { get; set; }
         public DbSet<Motocicletas> Motocicletas { get; set; }
