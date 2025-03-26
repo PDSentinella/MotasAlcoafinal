c357a546cbd94f8a00d46086e468b3af49156fae
Paulo <124280407+PDSentinella@users.noreply.github.com>
2025-03-26 19:48:34 +0000

    commit

--- Controllers/MotorcicletasController.cs
+++ Controllers/MotorcicletasController.cs
@@ -30,6 +30,60 @@ namespace MotasAlcoafinal.Controllers
 
             return View(motocicletas);
         }
+        public async Task<IActionResult> Details(int id)
+        {
+            var motocicleta = await _context.Motocicletas.FindAsync(id);
+            if (motocicleta == null)
+            {
+                return NotFound();
+            }
+            return View(motocicleta);
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var motocicleta = await _context.Motocicletas.FindAsync(id);
+            if (motocicleta == null)
+            {
+                return NotFound();
+            }
+            return View(motocicleta);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Motocicletas motocicleta)
+        {
+            if (id != motocicleta.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(motocicleta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MotocicletaExists(motocicleta.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(motocicleta);
+        }
+        private bool MotocicletaExists(int id)
+        {
+            return _context.Motocicletas.Any(e => e.Id == id);
+        }
 
         public IActionResult Create()
         {
