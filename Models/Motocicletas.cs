54b9cb8579d88340088c4664495dbb25e895ea49
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-04-09 12:00:41 +0100

    Sumários para os atributos dos models.
    - Foram feitos sumários para os atributos dos models

--- Models/Motocicletas.cs
+++ Models/Motocicletas.cs
@@ -10,17 +10,41 @@ namespace motasAlcoafinal.Models
     /// </summary>
     public class Motocicletas
     {
+
+        /// <summary>
+        /// Identificador do motociclo
+        /// </summary>
+        [Key]
         public int Id { get; set; }
         public int? ClienteId { get; set; }
+
+        /// <summary>
+        /// A marca do motociclo
+        /// </summary>
         public string ?  Marca { get; set; }
+
+        /// <summary>
+        /// Modelo do motociclo
+        /// </summary>
         public string ? Modelo { get; set; }
+
+        /// <summary>
+        /// Ano de fabricação do motociclo
+        /// </summary>
         public int ?  Ano { get; set; }
+
+        /// <summary>
+        /// Placa de identificação do motociclo
+        /// </summary>
         public string ? Placa { get; set; }
 
         // Relacionamento: Uma motocicleta pertence a um cliente
         public Clientes? Cliente { get; set; }
 
         // Relacionamento: Uma motocicleta pode ter vários serviços
+        /// <summary>
+        /// Lista dos serviços que estão associados a um motociclo
+        /// </summary>
         public ICollection<Servicos>? Servicos { get; set; }
     }
 }
