939ac26823f683a3b57e3c61677fcd4eed73505e
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-05-03 12:44:14 +0100

    Implementação do recuperador de password
    - Implementação do recuperador de password através do email.

---
+++ Models/ResetPasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotasAlcoafinal.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "As passwords não coincidem.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
