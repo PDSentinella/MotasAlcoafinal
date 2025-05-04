a665ec810d7c617ad424ceec522b741379c93df5
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-05-04 11:53:13 +0100

    Implementação das user Roles
    -Implementação das user Roles, para garantir a autorização aos acessos das páginas.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -13,13 +13,14 @@ namespace MotasAlcoafinal.Controllers
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly EmailService _emailService;
+        private readonly RoleManager<IdentityRole> _roleManager; 
 
-        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, EmailService emailService)
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, EmailService emailService, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _emailService = emailService;
-            
+            _roleManager = roleManager;
         }
 
         [HttpGet]
@@ -104,6 +105,42 @@ namespace MotasAlcoafinal.Controllers
 
             return View(model);
         }
+
+
+
+        [HttpGet]
+        public IActionResult ResetPasswordConfirmation()
+        {
+            return View(); 
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(string userId, string role)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound("Utilizador não encontrado.");
+            }
+
+            if (!await _roleManager.RoleExistsAsync(role))
+            {
+                return BadRequest("A role não existe.");
+            }
+
+            var result = await _userManager.AddToRoleAsync(user, role);
+            if (result.Succeeded)
+            {
+                return Ok($"Role '{role}' atribuída ao utilizador '{user.UserName}' com sucesso.");
+            }
+
+            return BadRequest("Erro ao atribuir a role.");
+        }
+
+
+
+
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
