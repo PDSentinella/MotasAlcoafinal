7c8768afab5ee64ed66a93f4695983fdeb0bd5d6
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-05-28 10:14:25 +0100

    Criação da API e alterações ao clientesController da pasta API
    Criação da API e alterações ao clientesController da pasta API

---
+++ Controllers/API/ClientesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MotasAlcoafinal.Models;
+using motasAlcoafinal.Models;
+
+namespace MotasAlcoafinal.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly MotasAlcoaContext _context;
+
+        public ClientesController(MotasAlcoaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clientes
+        /// <summary>
+        /// Devolve a lista com todas as categorias
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Clientes>>> GetClientes()
+        {
+            return await _context.Clientes.ToListAsync();
+        }
+
+
+        /// <summary>
+        /// GET: Devolver um cliente, quando a solicitação é feita através de HTTP GET
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET: api/Clientes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Clientes>> GetCliente(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return cliente;
+        }
+
+
+        /// <summary>
+        /// Edição de um cliente
+        /// </summary>
+        /// <param name="id"> identificação do cliente a editar</param>
+        /// <param name="cliente">Novos dados do cliente</param>
+        /// <returns></returns>
+        // PUT: api/Clientes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCliente(int id, Clientes cliente)
+        {
+            if (id != cliente.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cliente).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExiste(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Adição de um cliente
+        /// </summary>
+        /// <param name="cliente">Dados do cliente a adicionar</param>
+        /// <returns></returns>
+        // POST: api/Clientes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Clientes>> PostCliente(Clientes cliente)
+        {
+            _context.Clientes.Add(cliente);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClientes", new { id = cliente.Id }, cliente);
+        }
+
+        // DELETE: api/Clientes/5
+        /// <summary>
+        /// Apagar um cliente
+        /// </summary>
+        /// <param name="id">Identificador do cliente a apagar</param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCliente(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clientes.Remove(cliente);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Determina se a categoria existe
+        /// </summary>
+        /// <param name="id">Identificador do cliente a procurar</param>
+        /// <returns>'true' se o cliente existe, senão 'false'</returns>
+        private bool ClienteExiste(int id)
+        {
+            return _context.Clientes.Any(e => e.Id == id);
+        }
+    }
+}
