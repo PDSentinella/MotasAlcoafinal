8456e50ce651e5cb07941ee532d07c211e3eae37
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-05-28 11:40:59 +0100

    Configurar app para usar tokens jwt
    Configurar app para usar tokens jwt.

---
+++ Controllers/API/MotocicletasAuthController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MotasAlcoafinal.Models;
+using motasAlcoafinal.Models;
+using MotasAlcoafinal.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MotasAlcoafinal.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize] //podemos meter com ROLES
+    public class MotocicletasAuthController : ControllerBase
+    {
+        private readonly MotasAlcoaContext _context;
+
+        public MotocicletasAuthController(MotasAlcoaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Motocicletas
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<MotocicletasDTO>>> GetMotocicletas()
+        {
+
+            //return await _context.Motocicletas.ToListAsync();
+
+            var listagemFotos = await _context.Motocicletas
+                .OrderByDescending(m=>m.Ano)
+                .Select(m=> new MotocicletasDTO
+                {
+                    Marca=m.Marca,
+                    Modelo=m.Modelo,
+                    Ano=m.Ano,
+                })
+                .ToListAsync();
+
+            return listagemFotos;
+        }
+
+        // GET: api/Motocicletas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MotocicletasDTO>> GetMotocicleta(int id)
+        {
+            var motocicleta = await _context.Motocicletas
+                                            .Where(m=>m.Id == id)
+                                            .Select(m=> new MotocicletasDTO{
+                                             Marca = m.Marca,
+                                             Modelo=m.Modelo,
+                                             Ano=m.Ano,
+                                            }).FirstOrDefaultAsync();
+
+            if (motocicleta == null)
+            {
+                return NotFound();
+            }
+
+            return motocicleta;
+        }
+
+        // PUT: api/Motocicletas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMotocicleta(int id, Motocicletas motocicleta)
+        {
+            if (id != motocicleta.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(motocicleta).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MotocicletasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Motocicletas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Motocicletas>> PostMotocicleta(Motocicletas motocicleta)
+        {
+            _context.Motocicletas.Add(motocicleta);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMotocicletas", new { id = motocicleta.Id }, motocicleta);
+        }
+
+        // DELETE: api/Motocicletas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMotocicleta(int id)
+        {
+            var motocicleta = await _context.Motocicletas.FindAsync(id);
+            if (motocicleta == null)
+            {
+                return NotFound();
+            }
+
+            _context.Motocicletas.Remove(motocicleta);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MotocicletasExists(int id)
+        {
+            return _context.Motocicletas.Any(e => e.Id == id);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using MotasAlcoafinal.Models;
 using MotasAlcoafinal.Services;
 using MotasAlcoafinal.Data;
+using System.Text.Json.Serialization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +35,45 @@ builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Account/Login"; // Redireciona para login se não autenticado
 });
+
+// Eliminar a proteção de 'ciclos' qd se faz uma pesquisa que envolva um relacionamento 1-N em Linq
+// https://code-maze.com/aspnetcore-handling-circular-references-when-working-with-json/
+// https://marcionizzola.medium.com/como-resolver-jsonexception-a-possible-object-cycle-was-detected-27e830ea78e5
+builder.Services.AddControllers()
+                .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+// *******************************************************************
+// Instalar o package
+// Microsoft.AspNetCore.Authentication.JwtBearer
+//
+// using Microsoft.IdentityModel.Tokens;
+// *******************************************************************
+// JWT Settings
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(options => { })
+   .AddCookie("Cookies", options => {
+       options.LoginPath = "/Identity/Account/Login";
+       options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+   })
+   .AddJwtBearer("Bearer", options => {
+       options.TokenValidationParameters = new TokenValidationParameters
+       {
+           ValidateIssuer = true,
+           ValidateAudience = true,
+           ValidateLifetime = true,
+           ValidateIssuerSigningKey = true,
+           ValidIssuer = jwtSettings["Issuer"],
+           ValidAudience = jwtSettings["Audience"],
+           IssuerSigningKey = new SymmetricSecurityKey(key)
+       };
+   });
+
+
+// configuração do JWT
+builder.Services.AddScoped<TokenService>();
+
 
 var app = builder.Build();
 
