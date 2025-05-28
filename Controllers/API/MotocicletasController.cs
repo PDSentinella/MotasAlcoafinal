a18d8d61a8f28e29ae41785b03b2e95213e6b884
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-05-28 10:20:47 +0100

    Adição da classe API - motocicletas

---
+++ Controllers/API/MotocicletasController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MotasAlcoafinal.Models;
+using motasAlcoafinal.Models;
+
+namespace MotasAlcoafinal.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MotocicletasController : ControllerBase
+    {
+        private readonly MotasAlcoaContext _context;
+
+        public MotocicletasController(MotasAlcoaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Motocicletas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Motocicletas>>> GetMotocicletas()
+        {
+            return await _context.Motocicletas.ToListAsync();
+        }
+
+        // GET: api/Motocicletas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Motocicletas>> GetMotocicleta(int id)
+        {
+            var motocicleta = await _context.Motocicletas.FindAsync(id);
+
+            if (motocicleta == null)
+            {
+                return NotFound();
+            }
+
+            return motocicleta;
+        }
+
+        // PUT: api/Motocicletas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMotocicleta(int id, Motocicletas motocicleta)
+        {
+            if (id != motocicleta.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(motocicleta).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MotocicletasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Motocicletas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Motocicletas>> PostMotocicleta(Motocicletas motocicleta)
+        {
+            _context.Motocicletas.Add(motocicleta);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMotocicletas", new { id = motocicleta.Id }, motocicleta);
+        }
+
+        // DELETE: api/Motocicletas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMotocicleta(int id)
+        {
+            var motocicleta = await _context.Motocicletas.FindAsync(id);
+            if (motocicleta == null)
+            {
+                return NotFound();
+            }
+
+            _context.Motocicletas.Remove(motocicleta);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MotocicletasExists(int id)
+        {
+            return _context.Motocicletas.Any(e => e.Id == id);
+        }
+    }
+}
